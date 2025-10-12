fe0f5018211425aba4ab78c6fb77004d28d5b646
Magdesyan Kirill <magdesyankirill@gmail.com>
2025-10-12 18:36:40 +0300

    Описание изменений

--- RecipeBookMiniApp/src/App.jsx
+++ RecipeBookMiniApp/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import NotFoundPage from './pages/NotFoundPage';
 import RecipeDetailPage from './pages/RecipeDetailPage';
 import RecipeListPage from './pages/RecipeListPage';
-
+import { NavLink } from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <header>
+        <NavLink to="/">Главная</NavLink>
+        <NavLink to="/recipes">Рецепты</NavLink>
+      </header>
       <Routes>
+        <Route path="/" element={<RecipeListPage />}/>
         <Route path="/recipes" element={<RecipeListPage />}/>
         <Route path="/recipes/:recipeId" element={<RecipeDetailPage />}/>
-        <Route path="/*" element={<NotFoundPage />}/>
+        <Route path="*" element={<NotFoundPage />}/>
       </Routes>
     </>
   );
